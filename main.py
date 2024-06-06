1ef89d040b39ddb523e20299c01b1c071c253f90
pedro henrique <lopesph06@gmail.com>
2024-06-06 11:48:17 -0300

    nova versao da tela de login

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def login():
-    return 'Tela de login'
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
