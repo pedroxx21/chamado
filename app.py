d1754807e0c61b481075427ee5204cbf236b61d5
pedro henrique <lopesph06@gmail.com>
2024-06-18 10:42:12 -0300

    dia 18 2 atualizado

--- app.py
+++ app.py
@@ -1,38 +1,63 @@
-from flask import Flask, render_template, request, session, redirect, jsonify, url_for
+from flask import Flask, render_template, request, session,redirect, jsonify,url_for
+from flask_wtf import CSRFProtect 
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 from datetime import date
 
 app = Flask(__name__)
+app.secret_key = b'40203b1ce74ad29c891d33f0afef4d2c'
+csrf = CSRFProtect(app) 
+bcrypt = Bcrypt(app)
 
+'''
+    Configurações da string de conexão
+'''
 app.config['MYSQL_HOST'] = 'db-mysql-nyc3-13529-do-user-15782594-0.c.db.ondigitalocean.com'
 app.config['MYSQL_USER'] = 'doadmin'
 app.config['MYSQL_PASSWORD'] = 'AVNS_be6DYBLeXon1FMWpWWZ'
-app.config['MYSQL_DB'] = 'db_pedro'
+app.config['MYSQL_DB'] = 'db_professor'
 app.config['MYSQL_PORT'] = 25060
 
 mysql = MySQL(app)
 
-app.secret_key = 'ola brasil amado'
-bcrypt = Bcrypt(app)
+#app.secret_key = 'ola brasil amado'
 
-@app.route('/')
+
+
+'''
+    Rota principal da aplicação
+'''
+@app.route("/")
 def login():
     return render_template('login.html', is_home=True)
 
-@app.route('/dashboard')
+
+'''
+    Rota da dashboard
+'''
+@app.route("/dashboard")
 def dashboard():
-    return render_template('dashboard.html', is_home=True)
+    return render_template('dashboard.html',is_home=False)
 
-@app.route('/consumidor/formulario')
+
+
+'''
+    Rota do formulario
+'''
+@app.route("/consumidor/formulario")
 def formulario():
-    return render_template('formulario.html', is_home=True)
+    return render_template('formulario.html', is_home=False)
 
-@app.route('/consumidor/insert', methods = ['POST'])
+
+
+'''
+    Insert registros na tabela de tb_consumidor
+'''
+@app.route("/consumidor/insert", methods = ['POST'])
 def consumidorInsert():
 
     if request.method == 'POST':
-        nomeDoconsumidor = request.form['nomeDoConsumidor']
+        nomeDoConsumidor = request.form['nomeDoConsumidor']
         email = request.form['email']
         celular = request.form['celular']
         senha = request.form['senha']
@@ -41,11 +66,53 @@ def consumidorInsert():
         hashPassword = bcrypt.generate_password_hash(senha).decode('utf-8')
 
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO db_Consumidor (nome, email, celular, senha, created_at)VALUES(%s,%s,%s,%s,%s)', (nomeDoconsumidor, email, int(celular),hashPassword,dataAtual))
+        cur.execute("INSERT INTO tb_consumidor (nomeDoConsumidor, email, celular, senha, created_at) VALUES (%s, %s, %s, %s, %s)", (nomeDoConsumidor, email, int(celular), hashPassword, dataAtual))
         mysql.connection.commit()
 
-        return render_template('login.html', is_home=True, mensagem = 'consumidor')
-    
-    
+        return render_template('login.html', is_home=True,mensagem = "Consumidor inserido com sucesso")
+
+
+
+'''
+    Login do usuario
+'''
+@app.route("/login", methods = ['POST'])
+def loginConsumidor():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['password']
+
+        cur = mysql.connection.cursor()
+        cur.execute("Select * from tb_consumidor WHERE email= %s",(email, ))
+        usuario = cur.fetchone()
+        cur.close
+
+        return jsonify({'error': usuario}), 200
+
+        stored_password_hash = usuario[4]
+
+        if bcrypt.check_password_hash(stored_password_hash.encode('utf-8'), senha.encode('utf-8')):
+            session['user'] = usuario[1]
+            return redirect(url_for('dashboard'))
+        else:
+            return jsonify({'error': 'Invalid password'}), 401
+    else:
+        return jsonify({'error': 'User not found'}), 404
+        
+
+
+'''
+    Retornar todos os registros cadastrados na tabela de consumidor
+'''
+@app.route("/consumidor/lista")
+def listaConsumidor():
+    cur = mysql.connection.cursor()
+    cur.execute("Select * from tb_consumidor")
+    data = cur.fetchall()
+    cur.close()
+
+    return render_template('index.html', consumidor = data)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
